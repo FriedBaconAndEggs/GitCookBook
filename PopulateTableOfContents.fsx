8678ba6ca4525daa10ab408dd5cf4deecea7f9de
Nikodem Mazur <nikodem.mazur@gmail.com>
2021-01-07 21:54:11 +0100

    Update TOC

---
+++ PopulateTableOfContents.fsx
@@ -0,0 +1,41 @@
+open System.Text.RegularExpressions
+open System.IO;
+open System
+
+let (|Heading|_|) i =
+  let m = Regex.Match (i, @"(?m)^\s*(#)+\s+(.+)\r?$")
+  if m.Success then
+    Some (m.Groups.[1].Captures.Count, m.Groups.[2].Value)
+  else
+    None
+
+let parseHeading str =
+    match str with
+        | Heading (nesting, value) -> 
+            Some (String.replicate (nesting - 1) "\t" + 
+                sprintf "- [%s](#" value +
+                Regex.Replace (value, @"\s", "-") +
+                ")")
+        | _ -> None
+
+let parseTocText readmeLines =
+    readmeLines
+        |> Array.map parseHeading
+        |> Array.where (fun x -> x.IsSome)
+        |> Array.map (fun x -> x.Value)
+
+let updateToc (readme : string) (newTocText : string) =
+    Regex.Replace(readme, @"(?ms)(?<=#+\s+Table of contents\s*\r?\n).+?(?=^\s*#+\s*\w+)", newTocText)
+
+let removeCodeBlocks str =
+    Regex.Replace(str, @"(?ms)^```.+?```\r?$", "")
+
+let readmeText = File.ReadAllText "README.md"
+
+readmeText
+    |> removeCodeBlocks
+    |> (fun s -> Regex.Split(s, Environment.NewLine))
+    |> parseTocText
+    |> String.concat Environment.NewLine
+    |> updateToc readmeText
+    |> printfn "%s"
